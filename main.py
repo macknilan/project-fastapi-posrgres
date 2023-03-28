43ed97dc6229b032d79b27d778bc394811cde9df
Rodolfo <rodolfougaldeochoa@gmail.com>
2023-03-27 23:39:57 -0600

    :construction: [MOD] retornar pagina HTML

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+from builtins import bytes
 from wsgiref.simple_server import make_server
 
 
+HTML5 = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Servidor python</title>
+</head>
+<body>
+    <h1>Servidor en python</h1>
+</body>
+</html>
+"""
+
+
 def application(env, start_response):
-    headers = [("Content-Type", "text/plain")]
+    headers = [("Content-Type", "text/html")]
     start_response("200 Ok", headers)
 
-    return ["Hola mundo, desde mi primer servidor en python".encode("utf-8")]
+    return [bytes(HTML5, "utf-8")]
 
 
 server = make_server("localhost", 8000, application)
