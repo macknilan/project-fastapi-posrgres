90252751e36b09fed9785155100435ed10b8cf5f
Rodolfo <rodolfougaldeochoa@gmail.com>
2023-03-28 18:38:18 -0600

    :construction: WIP cliente en python

---
+++ cliente.py
@@ -0,0 +1,15 @@
+from urllib import request
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+URL = "http://localhost:8000/"
+
+
+response = request.urlopen(URL)
+logger.info(f" response -----> {response}")
+# print(f" response -----> {response.__dict__}")
+print(f" response -----> {response.read()}")
+
+
